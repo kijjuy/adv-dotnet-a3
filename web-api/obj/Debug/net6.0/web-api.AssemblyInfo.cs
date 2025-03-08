5438523e630f16ef9c73adb4d160562eb25fd82e
Brandon Zomer <brandonzomer@proton.me>
2025-03-08 13:28:42 -0500

    add models folder

--- web-api/obj/Debug/net6.0/web-api.AssemblyInfo.cs
+++ web-api/obj/Debug/net6.0/web-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b351797099de9f63d1007c3d5bbf94aee7eb279")]
 [assembly: System.Reflection.AssemblyProductAttribute("web-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
