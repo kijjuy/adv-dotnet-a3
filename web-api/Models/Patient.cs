6e126df26909cdfb9895521d3a0277507691e438
Brandon Zomer <brandonzomer@proton.me>
2025-03-08 13:54:17 -0500

    add Patient model

---
+++ web-api/Models/Patient.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace web_api;
+
+public class Patient
+{
+
+    [Key]
+    [Required]
+    public Guid Id { get; set; }
+
+    [Required]
+    public DateTimeOffset CreationTime { get; set; }
+
+    [Required]
+    [StringLength(128)]
+    public String FirstName { get; set; }
+
+    [Required]
+    [StringLength(128)]
+    public String LastName { get; set; }
+
+    [Required]
+    public DateTimeOffset DateOfBirth { get; set; }
+}
